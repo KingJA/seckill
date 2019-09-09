4cf69ba2072647301b6ef329c835170e689d1719
KingJA <kingjavip@gmail.com>
2019-09-09 17:28:28 +0800

    3

--- src/main/java/com/kingja/seckill/dao/OrderDao.java
+++ src/main/java/com/kingja/seckill/dao/OrderDao.java
@@ -2,13 +2,12 @@ package com.kingja.seckill.dao;
 
 import com.kingja.seckill.domain.MiaoshaOrder;
 import com.kingja.seckill.domain.OrderInfo;
-import com.kingja.seckill.vo.GoodsVo;
 
+import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
 import org.apache.ibatis.annotations.Select;
-
-import java.util.List;
+import org.apache.ibatis.annotations.SelectKey;
 
 /**
  * Description:TODO
@@ -22,8 +21,10 @@ public interface OrderDao {
 
     @Select("select from miaosha_order where user_id=#{userId} and goods_id=#{goodsId} ")
     MiaoshaOrder getMiaoshaOrderByUserIdGoodsId(@Param("userId") long userId, @Param("goodsId") long goodsId);
-
+    @Insert("insert into order_info(user_id, goods_id, goods_name, goods_count, goods_price, order_channel, status, create_date)values("
+            + "#{userId}, #{goodsId}, #{goodsName}, #{goodsCount}, #{goodsPrice}, #{orderChannel},#{status},#{createDate} )")
+    @SelectKey(keyColumn="id", keyProperty="id", resultType=long.class, before=false, statement="select last_insert_id()")
     long insert(OrderInfo orderInfo);
-
+    @Insert("insert into miaosha_order (user_id, goods_id, order_id)values(#{userId}, #{goodsId}, #{orderId})")
     int insertMiaoshaOrder(MiaoshaOrder miaoshaOrder);
 }
